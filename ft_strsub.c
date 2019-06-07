b86b95ae9744e7e5e77dff098030e86b1d2f4bf2
jde-beer <50702577+jde-beer@users.noreply.github.com>
2019-06-07 13:17:27 +0200

    Add files via upload

---
+++ ft_strsub.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jde-beer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/07 09:05:41 by jde-beer          #+#    #+#             */
+/*   Updated: 2019/06/07 13:09:09 by jde-beer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*j;
+	size_t	p;
+
+	if (!(j = (char *)malloc(sizeof(char) *(ft_strlen(s) + 1))))
+		return (NULL);
+	p = 0;
+	while (len < p && s[p + start] != '\0')
+	{
+		j[p] = s[start + p];
+		p++;
+	}
+	j[p] = '\0';
+	return (j);
+}
