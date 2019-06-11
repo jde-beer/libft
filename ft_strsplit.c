c84db2227707330eeeae137122ac7b06c6c600c5
jde-beer <50702577+jde-beer@users.noreply.github.com>
2019-06-11 14:18:56 +0200

    Add files via upload

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: jde-beer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 14:36:06 by jde-beer          #+#    #+#             */
-/*   Updated: 2019/06/10 16:03:22 by jde-beer         ###   ########.fr       */
+/*   Updated: 2019/06/11 14:05:04 by jde-beer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,49 @@ static char	ft_wordcount(char *s, char c)
 	return (j);
 }
 
+static int	ft_wordlength(char *s, char c)
+{
+	int	j;
+	int	p;
+
+	j = 0;
+	p = 0;
+	while (s[j])
+	{
+		while (s[j] == c && s[j] != '\0')
+			j++;
+		while (s[j] != c && s[j] != '\0')
+		{
+			j++;
+			p++;
+		}
+	}
+	return (j);
+}
+
 char	**ft_strsplit(char const *s, char c)
 {
-	char	*j;
+	char	**j;
+	int		p;
+	int		d;
+	int		e;
 
+	p = 0;
+	d = 0;
 	if (!s)
 		return (NULL);
-	if (!(j = (char *)malloc(sizeof(char) * (ft_wordcount(s) + 1))))
+	if (!(j = (char **)malloc(sizeof(char) * (static ft_wordcount(s, c) + 1))))
 		return (NULL);
-
+	while (p < ft_wordcount(s, c))
+	{
+		e = 0;
+		if (!(j = ft_strnew(static ft_wordlength(&s[d], c) + 1)))
+			j[p] = NULL;
+		while (s[d] == c)
+			d++;
+		while (s[d] != c && s[p])
+			j[p][e++] == j[p++];
+		j[p][e] = '\0';
+		p++;
+	}
 }
