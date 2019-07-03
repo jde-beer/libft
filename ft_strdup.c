5bdfd04bbd6c2b520f5567c3d4af79cf3f81878e
jde-beer <50702577+jde-beer@users.noreply.github.com>
2019-07-03 12:25:00 +0200

    Add files via upload

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,13 +6,13 @@
 /*   By: jde-beer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 14:46:12 by jde-beer          #+#    #+#             */
-/*   Updated: 2019/06/18 09:31:07 by jde-beer         ###   ########.fr       */
+/*   Updated: 2019/07/03 12:20:27 by jde-beer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(char *src)
+char	*ft_strdup(const char *src)
 {
 	int		i;
 	char	*dest;
